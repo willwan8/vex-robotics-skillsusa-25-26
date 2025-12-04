dbe89a7926c790050110a6c272d00b4e4e758535
willwan8 <williamwang1562@gmail.com>
2025-12-03 23:15:08 -0600

    finished driver controls

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -13,14 +13,15 @@ void setSpeedIntakeBottom(int speed) {
 }
 
 double averageImuHeading(double h1, double h2) {
-    // first convert to radians to prevent naive averaging
+    // convert to unit circle values to prevent naive averaging
     // ex of naive averaging: 359 + 1 => avg of 180, but actual avg is 0!
+    // but if we converted them into unit circle value first, then we'd get an avg of 0!
     // then add together to get an angle in the middle of h1 and h2 (basically an average)
     double xSum = cos((h1 * (2 * M_PI)) / 360.0) + cos((h2 * (2 * M_PI)) / 360.0);
     double ySum = sin((h1 * (2 * M_PI)) / 360.0) + sin((h2 * (2 * M_PI)) / 360.0);
 
     // convert back to degrees
-    double angle = (atan2(ySum / xSum) * 360.0) / (2*M_PI);
+    double angle = (atan2(ySum, xSum) * 360.0) / (2*M_PI);
     
     // convert angles from [0, 360]
     // add 360 in case of negatives
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "pros/abstract_motor.hpp"
 #include "pros/adi.hpp"
 #include "global.cpp"
+#include "helpers.cpp"
 
 /* CONTROLLER */
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
@@ -119,23 +120,32 @@ void initialize() {
     chassis.calibrate();   // calibrates wheels + odometry
     pros::lcd::set_text(2, "Chassis calibrated!");
 
-    // --- DEBUG TASK ---
+		pros::lcd::set_text(0, "Done initializing!");
+		pros::delay(1000); // so the message can appear on screen before telemetry
+
+    // --- TELEMETRY TASK ---
     // prints position + telemetry to LCD every 50ms
     pros::Task screenTask([&]() {
         while (true) {
             lemlib::Pose pose = chassis.getPose();
 
-            pros::lcd::print(0, "X: %.2f", pose.x);
-            pros::lcd::print(1, "Y: %.2f", pose.y);
-            pros::lcd::print(2, "T: %.2f", pose.theta);
+            pros::lcd::print(0, "X: %f", pose.x);
+            pros::lcd::print(1, "Y: %f", pose.y);
+            pros::lcd::print(2, "Theta: %f", pose.theta);
 
-            lemlib::telemetrySink()->info("Pose: {}", chassis.getPose());
+            pros::lcd::print(3, "IMU1 Heading: %f", imu.get_heading());
+						pros::lcd::print(4, "IMU2 Heading: %f", imu2.get_heading());
+						pros::lcd::print(5, "AVG IMU Heading: %f", averageImuHeading(imu.get_heading(), imu2.get_heading()));
+						pros::lcd::print(6, "IMU1 Orientation: %f", imu.get_physical_orientation());
+						pros::lcd::print(7, "IMU2 Orientation: %f", imu2.get_physical_orientation());
+
+						pros::lcd::print(8, "Rotation Sensor: %i", verticalEncoder.get_position());
 
             pros::delay(100);
         }
     });
 
-    pros::lcd::set_text(3, "Done initializing!");
+    
 }
 
 /**
@@ -152,7 +162,7 @@ void competition_initialize() {
 
 // get a path used for pure pursuit
 // this needs to be put outside a function
-ASSET(example_txt); // '.' replaced with "_" to make c++ happy
+//ASSET(example_txt); // '.' replaced with "_" to make c++ happy
 
 /**
  * Runs during auto
@@ -160,32 +170,7 @@ ASSET(example_txt); // '.' replaced with "_" to make c++ happy
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-    // Move to x: 20 and y: 15, and face heading 90. Timeout set to 4000 ms
-    chassis.moveToPose(20, 15, 90, 4000);
-    // Move to x: 0 and y: 0 and face heading 270, going backwards. Timeout set to 4000ms
-    chassis.moveToPose(0, 0, 270, 4000, {.forwards = false});
-    // cancel the movement after it has traveled 10 inches
-    chassis.waitUntil(10);
-    chassis.cancelMotion();
-    // Turn to face the point x:45, y:-45. Timeout set to 1000
-    // dont turn faster than 60 (out of a maximum of 127)
-    chassis.turnToPoint(45, -45, 1000, {.maxSpeed = 60});
-    // Turn to face a direction of 90º. Timeout set to 1000
-    // will always be faster than 100 (out of a maximum of 127)
-    // also force it to turn clockwise, the long way around
-    chassis.turnToHeading(90, 1000, {.direction = AngularDirection::CW_CLOCKWISE, .minSpeed = 100});
-    // Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
-    // following the path with the back of the robot (forwards = false)
-    // see line 116 to see how to define a path
-    chassis.follow(example_txt, 15, 4000, false);
-    // wait until the chassis has traveled 10 inches. Otherwise the code directly after
-    // the movement will run immediately
-    // Unless its another movement, in which case it will wait
-    chassis.waitUntil(10);
-    pros::lcd::print(4, "Traveled 10 inches during pure pursuit!");
-    // wait until the movement is done
-    chassis.waitUntilDone();
-    pros::lcd::print(4, "pure pursuit finished!");
+    // code auton :)
 }
 
 /**
@@ -196,12 +181,26 @@ void opcontrol() {
     // loop to continuously update motors
     while (true) {
 
-        // get joystick positions
-        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-        // move the chassis with curvature drive
-        chassis.tank(leftY, rightX);
-        // delay to save resources
-        pros::delay(25);
+			/* CONTROLS */
+			// running intake motors forward/backward
+			setSpeedIntakeTop((controller.get_digital(DIGITAL_R1) - controller.get_digital(DIGITAL_R2)) * 115);
+			setSpeedIntakeBottom((controller.get_digital(DIGITAL_L1) - controller.get_digital(DIGITAL_L2)) * 115);
+
+			// pneumatic controls
+			if (controller.get_digital_new_press(DIGITAL_UP)) {
+				tongueMech.extend();
+			}
+			if (controller.get_digital_new_press(DIGITAL_DOWN)) {
+				tongueMech.retract();
+			}
+
+			/* DRIVING */
+      // get joystick positions
+      int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+      int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+      // move the chassis with curvature drive
+     	chassis.tank(leftY, rightX);
+      // delay to save resources
+      pros::delay(25);
     }
 }
